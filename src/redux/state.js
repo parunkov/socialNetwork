aff52380c063aeaee043f7c08f8e821cf17ece71
parunkov <parunkov@gmail.com>
2020-06-16 11:45:43 +0500

    38

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,7 @@
-import {rerender} from './../render';
+// import {rerender} from './../index';
+let rerender = () => {
+	console.log('State is changed');
+}
 
 const data = ['Andrey', 'Galina', 'Anna', 'Maxim', 'Tanya'];
 	const dialogsData = [];
@@ -59,5 +62,47 @@ export const updateMewMessage = (newMessage) => {
 	state.messages.newMessageText = newMessage;
 	rerender(state);
 }
+
+export const subscribe = (observer) => {
+	rerender = observer;
+}
+
+let store = {
+	_state: state,
+	addPost () {
+		const newPost = {};
+		newPost.id = 3;
+		newPost.text = state.profile.newPostText;
+		newPost.likesCount = 0;
+		state.profile.postsData.push(newPost);
+		state.profile.newPostText = '';
+		rerender(state);
+	},
+
+	updateNewPostText (newText) {
+		state.profile.newPostText = newText;
+		rerender(state);
+	},
+
+	addMessage () {
+		const newMessage = {};
+		newMessage.id = 4;
+		newMessage.message = state.messages.newMessageText;
+		state.messages.messagesData.push(newMessage);
+		state.messages.newMessageText = '';
+		rerender(state);
+	},
+
+	updateMewMessage (newMessage) {
+		state.messages.newMessageText = newMessage;
+		rerender(state);
+	},
+
+	subscribe (observer) {
+		rerender = observer;
+	}
+}
+
+export {store};
 
 export default state;
