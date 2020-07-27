218957043da5cfba3ccf271cd70fdf15ee02b6ef
parunkov <parunkov@gmail.com>
2020-07-27 14:27:38 +0500

    Все удалил

--- src/components/Settings/Settings.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Settings = () => {
-	return (
-		<div className="">Settings</div>
-	);
-}
-
-export default Settings;
--- src/components/common/Preloader/Preloader.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import preloader from '../../../assets/images/Spin-1s-200px.svg';
-import styles from './Preloader.module.css';
-
-const Preloader = (props) => {
-	return <img src={preloader} className={styles.preloader} alt="" />
-}
-
-export default Preloader;
