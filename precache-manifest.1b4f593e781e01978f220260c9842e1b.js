058a87bf01f213ca956f5966bae496c2392e984b
parunkov <parunkov@gmail.com>
2020-07-27 14:37:17 +0500

    Updates

---
+++ precache-manifest.1b4f593e781e01978f220260c9842e1b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82c720a3ac0aa27a184fd838047dbc54",
+    "url": "/parunkov/socialNetwork/index.html"
+  },
+  {
+    "revision": "2b12915ced0db2ba1829",
+    "url": "/parunkov/socialNetwork/static/css/3.f9c82f57.chunk.css"
+  },
+  {
+    "revision": "270d51a523f82b85fa65",
+    "url": "/parunkov/socialNetwork/static/css/4.8b904ba3.chunk.css"
+  },
+  {
+    "revision": "654ac3e4829efa985e71",
+    "url": "/parunkov/socialNetwork/static/css/main.7d3876a8.chunk.css"
+  },
+  {
+    "revision": "cc281064e00c66ea365e",
+    "url": "/parunkov/socialNetwork/static/js/2.b735ba66.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/parunkov/socialNetwork/static/js/2.b735ba66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b12915ced0db2ba1829",
+    "url": "/parunkov/socialNetwork/static/js/3.01a9bf7b.chunk.js"
+  },
+  {
+    "revision": "270d51a523f82b85fa65",
+    "url": "/parunkov/socialNetwork/static/js/4.0f75dbc8.chunk.js"
+  },
+  {
+    "revision": "654ac3e4829efa985e71",
+    "url": "/parunkov/socialNetwork/static/js/main.66b7ed35.chunk.js"
+  },
+  {
+    "revision": "84c34946d1d809fae91f",
+    "url": "/parunkov/socialNetwork/static/js/runtime-main.6ca85c35.js"
+  },
+  {
+    "revision": "c02e8f9b14b1520da9e170a41e65eeb3",
+    "url": "/parunkov/socialNetwork/static/media/Spin-1s-200px.c02e8f9b.svg"
+  },
+  {
+    "revision": "ccf8b8414c2e43bd0721cf1afbe70d20",
+    "url": "/parunkov/socialNetwork/static/media/i.ccf8b841.webp"
+  }
+]);
